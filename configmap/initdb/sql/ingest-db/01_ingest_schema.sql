ae51bb139f37b117e3a1e183f7108b0e5e5c5ad1
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-02-02 11:38:52 +0100

    Update queue database schema for better monitoring

--- configmap/initdb/sql/ingest-db/01_ingest_schema.sql
+++ configmap/initdb/sql/ingest-db/01_ingest_schema.sql
@@ -2,6 +2,7 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 ;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 ;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL' ;
 
+
 CREATE DATABASE qservIngest;
 USE qservIngest;
 
@@ -11,17 +12,43 @@ USE qservIngest;
 --
 -- The list of chunks to load inside a Qserv database
 -- Used as a queue by loader jobs
-CREATE TABLE `task` (
+CREATE TABLE `chunkfile` (
 
-  `chunk_id`              INTEGER             NOT NULL ,                  -- the id of the chunk to load
+  `id`                    INTEGER UNSIGNED    NOT NULL AUTO_INCREMENT,
+  `chunk_id`              INTEGER UNSIGNED    NOT NULL ,                  -- the id of the chunk to load
   `chunk_file_path`       VARCHAR(255)        NOT NULL ,                  -- the path of the chunk file to load
   `database`              VARCHAR(255)        NOT NULL ,                  -- the name of the target database
   `is_overlap`            BOOLEAN             NOT NULL ,                  -- is this file an overlap
-  `pod`                   VARCHAR(255)        DEFAULT NULL ,              -- the name of the pod which launch the ingest
   `table`                 VARCHAR(255)        NOT NULL ,                  -- the name of the target table
-  `start`                 TIMESTAMP           NULL ,                      -- the date when ingest start for this file
-  `succeed`               TIMESTAMP           NULL ,                      -- the date when ingest has succeeded for this file
+  `ingest_time`           TIMESTAMP           NULL ,                      -- the date when this file is ingested in the current transaction
 
-  PRIMARY KEY (`chunk_id`, `chunk_file_path`, `database`, `is_overlap`, `table`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`chunk_id`, `chunk_file_path`, `database`, `is_overlap`, `table`)
+)
+ENGINE = InnoDB;
+
+create table `chunkfile_transaction` (
+    `chunkfile_id`   INTEGER UNSIGNED    NOT NULL ,
+    `transaction_id` INTEGER UNSIGNED    NOT NULL ,
+
+    UNIQUE KEY (`chunkfile_id`, `transaction_id`),
+
+    CONSTRAINT `transaction_fk1`
+      FOREIGN KEY (`transaction_id`)
+      REFERENCES `transaction` (`id`),
+
+    CONSTRAINT `chunkfile_fk1`
+      FOREIGN KEY (`chunkfile_id`)
+      REFERENCES `chunkfile` (`id`),
+)
+ENGINE = InnoDB;
+
+CREATE TABLE `transaction` (
+
+  `id`         INTEGER      NOT NULL ,                  -- the id of a replication service super-transaction
+  `pod`        VARCHAR(255) NOT NULL ,                  -- the id of the pod which run the transaction
+  -- `state`      VARCHAR(255) NOT NULL ,                  -- the latest returned state from the replication REST service
+  -- `begin_time` TIMESTAMP    NOT NULL ,                  -- the date when the replication REST service returns when starting a super-transaction
+  -- `end_time`   TIMESTAMP    DEFAULT NULL ,              -- the date when the replication REST service returns when stopping a super-transaction
 )
 ENGINE = InnoDB;
