a2d1f8803986aad4df11852c6c966f256b39143a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-03-02 06:49:35 +0100

    upd

--- configmap/initdb/sql/ingest-db/01_ingest_schema.sql
+++ configmap/initdb/sql/ingest-db/01_ingest_schema.sql
@@ -12,30 +12,30 @@ USE qservIngest;
 --
 -- The list of chunks to load inside a Qserv database
 -- Used as a queue by loader jobs
-CREATE TABLE `chunkfile` (
+CREATE TABLE `chunkfile_queue` (
 
   `id`                    INTEGER UNSIGNED    NOT NULL AUTO_INCREMENT,
   `chunk_id`              INTEGER UNSIGNED    NOT NULL ,                  -- the id of the chunk to load
   `chunk_file_path`       VARCHAR(255)        NOT NULL ,                  -- the path of the chunk file to load
   `database`              VARCHAR(255)        NOT NULL ,                  -- the name of the target database
   `is_overlap`            BOOLEAN             NOT NULL ,                  -- is this file an overlap
+  `locking_pod`           VARCHAR(255)        NOT NULL ,                  -- the id of the latest pod which has locked the chunk
+  `succeed`               BOOLEAN             NULL ,                      -- the status of the file:
+                                                                          --   - NULL (pending),
+                                                                          --   - 0 (error during latest ingest attempt),
+                                                                          --   - 1 (success during latest ingest attempt)
   `table`                 VARCHAR(255)        NOT NULL ,                  -- the name of the target table
-  `ingest_time`           TIMESTAMP           NULL ,                      -- the date when this file is ingested in the current transaction
 
   PRIMARY KEY (`id`),
   UNIQUE KEY (`chunk_id`, `chunk_file_path`, `database`, `is_overlap`, `table`)
 )
 ENGINE = InnoDB;
 
-create table `chunkfile_transaction` (
+create table `ingest_history` (
     `chunkfile_id`   INTEGER UNSIGNED    NOT NULL ,
+    `ingest_time`    TIMESTAMP           NULL ,                            -- the date when this file is ingested in the current transaction
+    `pod`            VARCHAR(255)        NOT NULL ,                        -- the id of the pod which will ingest the chunk file
     `transaction_id` INTEGER UNSIGNED    NOT NULL ,
-
-    UNIQUE KEY (`chunkfile_id`, `transaction_id`),
-
-    CONSTRAINT `transaction_fk1`
-      FOREIGN KEY (`transaction_id`)
-      REFERENCES `transaction` (`id`),
 
     CONSTRAINT `chunkfile_fk1`
       FOREIGN KEY (`chunkfile_id`)
@@ -43,12 +43,3 @@ create table `chunkfile_transaction` (
 )
 ENGINE = InnoDB;
 
-CREATE TABLE `transaction` (
-
-  `id`         INTEGER      NOT NULL ,                  -- the id of a replication service super-transaction
-  `pod`        VARCHAR(255) NOT NULL ,                  -- the id of the pod which run the transaction
-  -- `state`      VARCHAR(255) NOT NULL ,                  -- the latest returned state from the replication REST service
-  -- `begin_time` TIMESTAMP    NOT NULL ,                  -- the date when the replication REST service returns when starting a super-transaction
-  -- `end_time`   TIMESTAMP    DEFAULT NULL ,              -- the date when the replication REST service returns when stopping a super-transaction
-)
-ENGINE = InnoDB;
